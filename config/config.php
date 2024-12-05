5d61060193d51282c11d5b472bbda37c01a2134f
Erik <eri--345@hotmail.com>
2024-12-05 10:27:59 -0600

    cambios en login

--- config/config.php
+++ config/config.php
@@ -4,8 +4,14 @@ $usuario = "root";
 $contrasena = "";
 $base_de_datos = "ss_crud";
 
+// Crear conexión
 $conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
 
+// Verificar conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
+
+// Configuración para manejar caracteres especiales
+$conexion->set_charset("utf8mb4");
+?>
